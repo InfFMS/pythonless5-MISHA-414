cf8e12b4d86842338937f45f0588f5af0325af72
MISHA-414 <misha1.petruhin@gmail.com>
2024-10-27 22:51:04 +0700

    task6

--- task6.py
+++ task6.py
@@ -4,3 +4,18 @@
 # Пример: ввод N = 6
 # [1,2,3,4,5,6]
 # Вывод: [3,2,1,6,5,4]
+from random import randint
+def invers(list, len):
+    answer=[]
+    for i in range(-1, -1*len-1, -1):
+        answer.append(list[i])
+    return answer
+
+n=int(input())
+lst=[randint(1, 10) for i in range(n)]
+print(lst)
+
+left=lst[:n//2]
+right=lst[n//2:]
+
+print(invers(left, n//2), invers(right, n//2))
