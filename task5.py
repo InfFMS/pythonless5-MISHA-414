36702178b2626e350fd0b2204f3010c022d83723
MISHA-414 <misha1.petruhin@gmail.com>
2024-10-27 22:36:50 +0700

    task5

--- task5.py
+++ task5.py
@@ -1,2 +1,12 @@
 # Введите массив длины N с клавиатуры и найдите (за один проход)
 # количество элементов, имеющих максимальное значение.
+lst=list(map(int, input().split()))
+count= 0
+ma=lst[0]
+for i in range(len(lst)):
+    if lst[i]==ma:
+        count+=1
+    elif lst[i]>ma:
+        count=1
+        ma=lst[i]
+print(count)
