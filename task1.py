4bd0280d37ad5d7e10f411dff71e060847d49b97
Петрухин Михаил Дмитриевич <m.petrukhin@main.nsu.ru>
2024-10-28 14:02:33 +0700

    task1

--- task1.py
+++ task1.py
@@ -7,9 +7,27 @@ from random import sample
 # 4.YES, если список содержит трехзначное число, состоящее из одинаковых цифр
 # NO в противном случае;
 # 5.список с удаленными первым и последним элементами.
+def only_num(num):
+
+    while num>9:
+        if num%10!=num//10%10:
+            return 0
+        num=num//10
+    return 1
+
+
 nums=[i for i in range(1, 1001)]
 n=int(input())
-answer=sample(nums, n)
-print(answer)
+lst=sample(nums, n)
 
+print(n)
+print(lst[-1])
+print(lst[::-1])
 
+for num in lst:
+    if only_num(num) and num>99 and num<1000:
+        print('Yes')
+        break
+else:
+    print('No')
+print(lst[1:-1])
