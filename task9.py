3d27c54ac322e4f020cb32107fe5336855877188
Петрухин Михаил Дмитриевич <m.petrukhin@main.nsu.ru>
2024-10-28 13:34:28 +0700

    task9

--- task9.py
+++ task9.py
@@ -3,3 +3,9 @@
 # Пример: ввод N = 8
 # [10, 20, 10, 20, 30, 40, 30, 50]
 # После удаления дублей:  [10, 20, 30, 40, 50]
+lst=list(map(int, input().split()))
+sin_duble=[]
+for num in lst:
+    if not(num in sin_duble):
+        sin_duble.append(num)
+print(sin_duble)
