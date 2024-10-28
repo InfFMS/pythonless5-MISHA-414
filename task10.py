1489794db258c86eabdf720a8c8c131a16b9aa17
Петрухин Михаил Дмитриевич <m.petrukhin@main.nsu.ru>
2024-10-28 13:56:35 +0700

    task10

--- task10.py
+++ task10.py
@@ -11,15 +11,21 @@ n=int(input())
 lst_a=list(map(int, input().split()))
 lst_b=list(map(int, input().split()))
 summ=0
-len_a=1
-len_b=1
-for i in range(n):
-    a = lst_a[i]
-    b = lst_b[i]
-    summ += a*b
-    len_a += a**2
-    len_b += b**2
-cos = summ/(len_a*len_b)**0.5
-print(acos(cos)*180/pi)
+len_a=0
+len_b=0
+if n==0:
+    print('n=0  это точка')
+elif lst_a.count(0)==n or lst_b.count(0)==n:
+    print('один из векторов точка')
+else:
+    for i in range(n):
+        a = lst_a[i]
+        b = lst_b[i]
+        summ += a*b
+        len_a += a**2
+        len_b += b**2
+    cos = summ/(len_a*len_b)**0.5
+
+    print(acos(cos)*180/pi)
 
 
